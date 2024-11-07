73d25e27a89407ffd3953dbb5ab67fc32c14df30
wilson mwangi waweru <kouragitam@gmail.com>
2024-11-07 16:54:02 +0300

    adds corrections

--- server/app.py
+++ server/app.py
@@ -3,7 +3,7 @@ from flask_restful import Api, Resource, reqparse
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 from flask_migrate import Migrate
-from server.models import db # Import db directly
+from models import db # Import db directly
 import os
 
 app = Flask(__name__)
