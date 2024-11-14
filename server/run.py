1a29fb02d248fbce2e2751dc08b66a312995f9b1
wilson mwangi waweru <kouragitam@gmail.com>
2024-11-14 14:55:07 +0300

    adds api

--- server/run.py
+++ server/run.py
@@ -1,12 +1,8 @@
 import os
 from app import create_app, db
-from app.routes import api_bp
 
 # Create the application instance
 app = create_app()
-
-# Register blueprint for API routes
-app.register_blueprint(api_bp, url_prefix='/api')
 
 # Run the application
 if __name__ == '__main__':
