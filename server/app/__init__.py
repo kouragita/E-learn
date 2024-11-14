e609e0fd8ce04ee5f4a71a8fb721d5c8fb51298f
wilson mwangi waweru <kouragitam@gmail.com>
2024-11-14 14:10:03 +0300

    adds cors

--- server/app/__init__.py
+++ server/app/__init__.py
@@ -2,11 +2,12 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 from flask_migrate import Migrate
-
+from flask_cors import CORS
 # Initialize extensions
 db = SQLAlchemy()
 ma = Marshmallow()
 migrate = Migrate()
+
 
 def create_app():
     app = Flask(__name__)
@@ -18,6 +19,7 @@ def create_app():
     db.init_app(app)
     ma.init_app(app)
     migrate.init_app(app, db) 
+    CORS(app)
     
     # Import models here to register them with SQLAlchemy
     with app.app_context():
--- server/run.py
+++ server/run.py
@@ -1,15 +1,14 @@
-from app import create_app, db
-
-app = create_app()
-
-if __name__ == "__main__":
-    app.run(debug=True)
+import os
 from app import create_app, db
 from app.routes import api_bp
 
+# Create the application instance
 app = create_app()
+
+# Register blueprint for API routes
 app.register_blueprint(api_bp, url_prefix='/api')
 
+# Run the application
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5555))  # Use environment variable or default to 5555
+    port = int(os.environ.get("PORT", 5555)) 
     app.run(host="0.0.0.0", port=port, debug=True)
