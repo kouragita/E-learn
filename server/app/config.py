9ca9f22f96e270e9f9171f66b3f6b4bf29da06c1
wilson mwangi waweru <kouragitam@gmail.com>
2025-07-14 18:03:13 +0300

    updates database

--- server/app/config.py
+++ server/app/config.py
@@ -1,5 +1,5 @@
 class Config:
     DEBUG = True 
     # SQLALCHEMY_DATABASE_URI = 'sqlite:///e-learn.db'
-    SQLALCHEMY_DATABASE_URI = 'postgresql://crowdsourceddb_user:Vs9zYCGoE353F8r70QBCJTlakGqkuqrt@dpg-cvj3f424d50c73c73pdg-a.oregon-postgres.render.com/crowdsourceddb'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://crowdsourceddb_2q41_user:N9MsIvVcFBkeK00MlGe1iPBYcvRehvDt@dpg-d1qfqo3ipnbc7390ugdg-a.oregon-postgres.render.com/crowdsourceddb_2q41'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
